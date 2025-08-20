b6323ca2fe5a77ae48443d18930bc0a278ed9318
PU-MEriksson <malin.eriksson83@icloud.com>
2025-08-20 11:04:41 +0200

    Add styling to text

--- src/app/page.js
+++ src/app/page.js
@@ -7,10 +7,10 @@ export default function HomePage() {
     <main className={styles.container}>
       <section className={styles.section}>
         <Image src="/logo.svg" alt="App logo" width={115.094} height={122} />
-        <h1>FOTOGUESSR</h1>
-        <p>
-          Välkommen till Lindholmen Guesser! Testa din kunskap om Lindholmen
-          genom att gissa platser baserat på bilder.
+        <h1 className={styles.title}>FOTOGUESSR</h1>
+        <p className={styles.description}>
+          Gissa bilden, hitta platsen och tagga dig själv. Men låt inte dina
+          poäng ticka bort, för ju snabbare du är, desto fler poäng får du.
         </p>
         <Button href="/levels">Spela</Button>
         <Button href="/leaderboard">Poängställning</Button>
