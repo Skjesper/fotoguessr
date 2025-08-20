3e695d4aac51a7057f2529f8c53a0320c4714c7c
PU-MEriksson <malin.eriksson83@icloud.com>
2025-08-20 10:28:23 +0200

    Add html and button component to frontpage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,21 @@
 import styles from "./page.module.css";
+import Button from "@/components/Button";
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Lindholmen Guesser</h1>
+      <img
+        src="/logo.png"
+        alt="Lindholmen Guesser Logo"
+        className={styles.logo}
+      />
+      <h1>FOTOGUESSR</h1>
+      <p>
+        Välkommen till Lindholmen Guesser! Testa din kunskap om Lindholmen genom
+        att gissa platser baserat på bilder.
+      </p>
+      <Button href="/levels">Spela</Button>
+      <Button href="/leaderboard">Poängställning</Button>
     </div>
   );
 }
