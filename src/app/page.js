cad7e0f79ded45add2422f9b26d2bf0f47658530
PU-MEriksson <malin.eriksson83@icloud.com>
2025-08-20 09:47:04 +0200

    Remove test code from frontpage

--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,9 @@
-'use client';
-import { useState } from 'react';
-import Timer from '../components/Timer';
-import StreetViewDisplay from '../components/StreetViewDisplay';
+import styles from "./page.module.css";
 
-export default function Page() {
- const [progress, setProgress] = useState(0);
- const [timerFinished, setTimerFinished] = useState(false);
- 
- const testLocation = {
-   lat: 57.7068,
-   lng: 11.9373
- };
-
- const handleProgress = (progressValue) => {
-   setProgress(progressValue);
- };
-
- const handleComplete = () => {
-   setTimerFinished(true);
- };
-
- return (
-   <div>
-     <h1>Lindholmen Guesser</h1>
-     
-    <Timer 
-      duration={40}
-      onProgress={setProgress}  // Direkt utan wrapper
-      onComplete={() => setTimerFinished(true)}
-    />
-     
-     <StreetViewDisplay 
-       location={testLocation}
-       progress={progress}
-     />
-     
-     {timerFinished && (
-       <div>Tiden är slut!</div>
-     )}
-   </div>
- );
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Lindholmen Guesser</h1>
+    </div>
+  );
 }
