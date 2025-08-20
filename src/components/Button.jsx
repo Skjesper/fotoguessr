cf7c55237cb7cb288d34d3a9e75c6bbd2ae25c02
PU-MEriksson <malin.eriksson83@icloud.com>
2025-08-20 10:21:57 +0200

    Remade button component so it also works as a link

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,43 @@
 "use client";
+import Link from "next/link";
 import styles from "./Button.module.css";
+
+// Usage as a navigation button:
+// <Button href="/play">Play</Button>
+
+// Usage as an action button:
+// <Button onClick={handleStart}>Start Timer</Button>
 
 export default function Button({
   children,
+  href,
   onClick,
   variant = "primary",
   disabled = false,
+  ...props
 }) {
+  // If href prop is passed, render a Link (styled like a button)
+  if (href) {
+    return (
+      <Link
+        href={href}
+        className={styles.button}
+        data-variant={variant}
+        {...props}
+      >
+        {children}
+      </Link>
+    );
+  }
+
+  // Otherwise, render a real button
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      data-variant={variant} // If we want to style different variants in CSS later, can be removed if we don't need it
+      className={styles.button}
+      data-variant={variant}
+      {...props}
     >
       {children}
     </button>
