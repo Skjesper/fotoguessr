a449661170dd43e103fdb7dbbe1b399b7e3d44be
PU-MEriksson <malin.eriksson83@icloud.com>
2025-08-20 10:59:09 +0200

    Add logo

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,12 @@
 import styles from "./page.module.css";
 import Button from "@/components/Button";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
     <main className={styles.container}>
       <section className={styles.section}>
-        <img
-          src="/logo.png"
-          alt="Lindholmen Guesser Logo"
-          className={styles.logo}
-        />
+        <Image src="/logo.svg" alt="App logo" width={115.094} height={122} />
         <h1>FOTOGUESSR</h1>
         <p>
           Välkommen till Lindholmen Guesser! Testa din kunskap om Lindholmen
