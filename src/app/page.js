e457fe7fb7f2c08ea493ca4e29f615fd610be061
PU-MEriksson <malin.eriksson83@icloud.com>
2025-08-20 11:39:31 +0200

    Fix some positioning of elements

--- src/app/page.js
+++ src/app/page.js
@@ -6,22 +6,28 @@ export default function HomePage() {
   return (
     <main className={styles.container}>
       <section className={styles.section}>
-        <Image src="/logo.svg" alt="App logo" width={115.094} height={122} />
-        <h1 className={styles.title}>FOTOGUESSR</h1>
-        <p className={styles.description}>
-          Gissa bilden, hitta platsen och tagga dig själv. Men låt inte dina
-          poäng ticka bort, för ju snabbare du är, desto fler poäng får du.
-        </p>
-        <Button href="/levels" variant="dark">
-          Spela
-        </Button>
-        <Button
-          href="/leaderboard"
-          variant="dark"
-          icon={<Image src="/trophy.svg" alt="Trophy" width={16} height={16} />}
-        >
-          Poängställning
-        </Button>
+        <div className={styles.textImageContainer}>
+          <Image src="/logo.svg" alt="App logo" width={115.094} height={122} />
+          <h1 className={styles.title}>FOTOGUESSR</h1>
+          <p className={styles.description}>
+            Gissa bilden, hitta platsen och tagga dig själv. Men låt inte dina
+            poäng ticka bort, för ju snabbare du är, desto fler poäng får du.
+          </p>
+        </div>
+        <div className={styles.buttonContainer}>
+          <Button href="/levels" variant="dark">
+            Spela
+          </Button>
+          <Button
+            href="/leaderboard"
+            variant="dark"
+            icon={
+              <Image src="/trophy.svg" alt="Trophy" width={16} height={16} />
+            }
+          >
+            Poängställning
+          </Button>
+        </div>
       </section>
     </main>
   );
