632f82b9c1276fc594f1ebc53ac1c7d41f355f03
Skjesper <jesperskeppstedt@gmail.com>
2025-08-21 09:18:01 +0200

    Add icon to button and update StreetView image size
    Added an SVG icon to the 'Tagga plats' button for improved UI clarity. Increased the Google StreetView image width from 400px to 600px for better visibility. Removed unused header styles from gamePage CSS and made a minor formatting update in UnlockTime styles.

--- src/app/gamePage/page.js
+++ src/app/gamePage/page.js
@@ -53,12 +53,15 @@ export default function Page() {
         </div>
       </div>
       <div className= {styles.buttomUi}>
-      <Button 
-        variant="primary"
-        onClick={() => console.log('Button clicked')}
-      >
-       Tagga plats
-      </Button>
+    <Button 
+  variant="primary"
+  onClick={() => console.log('Button clicked')}
+>
+  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="23" viewBox="0 0 18 23" fill="none">
+    <path fillRule="evenodd" clipRule="evenodd" d="M9 0C4.02975 0 0 4.60355 0 9.77522C0 14.9066 2.87212 20.4843 7.35412 22.6256C7.86928 22.8722 8.43116 23 9 23C9.56884 23 10.1307 22.8722 10.6459 22.6256C15.1279 20.4843 18 14.9066 18 9.77522C18 4.60355 13.9703 0 9 0ZM9 11.5003C9.59674 11.5003 10.169 11.2579 10.591 10.8266C11.0129 10.3952 11.25 9.81021 11.25 9.2002C11.25 8.59019 11.0129 8.00517 10.591 7.57382C10.169 7.14248 9.59674 6.90015 9 6.90015C8.40326 6.90015 7.83097 7.14248 7.40901 7.57382C6.98705 8.00517 6.75 8.59019 6.75 9.2002C6.75 9.81021 6.98705 10.3952 7.40901 10.8266C7.83097 11.2579 8.40326 11.5003 9 11.5003Z" fill="#CBD9F4"/>
+  </svg>
+  Tagga plats
+</Button>
       </div>
       </section>
 
