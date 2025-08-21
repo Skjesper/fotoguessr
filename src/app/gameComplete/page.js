d54ce32fa51bcdde632496d7e7151e6f6b24f6bc
PU-MEriksson <malin.eriksson83@icloud.com>
2025-08-21 10:05:35 +0200

    Add html and finished components to page

--- src/app/gameComplete/page.js
+++ src/app/gameComplete/page.js
@@ -1,9 +1,28 @@
+import styles from "./page.module.css";
 import FotoguesserHeader from "@/components/FotoguesserHeader/FotoguesserHeader";
+import Button from "@/components/Button";
+
+let timeTaken = 5; // Example time taken in minutes
+let starsEarned = 2; // Example stars earned
 
 export default function GameCompletePage() {
   return (
     <main>
       <FotoguesserHeader />
+      <div className={styles.container}>
+        {/* Add stars component here */}
+        <h1 className={styles.title}>Grattis!</h1>
+        <p className={styles.description}>
+          Du klarade spelet med {timeTaken} minuter kvar och fick därför{" "}
+          {starsEarned} av 3 stjärnor!
+        </p>
+        <Button href="/" variant="light">
+          Hem
+        </Button>
+        <Button href="/leaderboard" variant="light">
+          Poängställning
+        </Button>
+      </div>
     </main>
   );
 }
